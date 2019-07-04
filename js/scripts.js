55f8214cc40df4fc2f1e7207b705851915ee6f29
Student <example@example.com>
2019-07-03 19:49:55 -0700

    update scripts to add up score and return unique alert

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 $(document).ready(function(){
   $("form#stress_test").submit(function(event){
     event.preventDefault();
-    $("#daily-signs").show();
+    // $("#daily-signs").show();
     $("input:checkbox[name=daily-signs]:checked").each(function(){
-      var dailySignsMode = $(this).val();
-      $('#daily-signs').append(dailySignsMode + "<br>");
+      var dailySignsMode = parseInt($(this).val());
+        if (dailySignsMode >= 6) {
+          alert("you are very stressed");
+      } else (dailySignsMode <=5)
+          alert ("you are not very stressed")
     });
-    $("#physiological-signs").show();
-    $("input:checkbox[name=physiological-signs]:checked").each(function(){
-      var physiologicalSignsMode = $(this).val();
-      $('#physiological-signs').append(physiologicalSignsMode + "<br>");
-    });
+
+    // $("#physiological-signs").show();
+    // $("input:checkbox[name=physiological-signs]:checked").each(function(){
+    //   var physiologicalSignsMode = $(this).val();
+    //   $('#physiological-signs').append(physiologicalSignsMode + "<br>");
+    // });
     $('#stress_test').hide();
   });
 });
