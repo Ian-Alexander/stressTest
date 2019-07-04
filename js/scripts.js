da0c0aa77a4794674bc7d3bf6a773caab01dd7cb
Student <example@example.com>
2019-07-03 20:42:48 -0700

    push all values into an array

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,23 @@
+var stressors =function (input){
+
+}
 $(document).ready(function(){
   $("form#stress_test").submit(function(event){
     event.preventDefault();
-    // $("#daily-signs").show();
+    var stress = [];
     $("input:checkbox[name=daily-signs]:checked").each(function(){
-      var dailySignsMode = parseInt($(this).val());
-        if (dailySignsMode >= 6) {
-          alert("you are very stressed");
-      } else (dailySignsMode <=5)
-          alert ("you are not very stressed")
-    });
+      var stressInput = $(this).val();
+      stress.push(stressInput);
 
+    //   var dailySignsMode = $(this).val();
+    //   console.log(dailySignsMode)
+    //     if (dailySignsMode >= 5) {
+    //       alert("you are very stressed");
+    //   } else {
+    //       alert ("you are not very stressed")
+    // }
+  });
+  console.log(stress);
     // $("#physiological-signs").show();
     // $("input:checkbox[name=physiological-signs]:checked").each(function(){
     //   var physiologicalSignsMode = $(this).val();
